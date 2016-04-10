aaf733d64e2640ba865b4ad48163f256565be1fd
loreLin <loreLinF@163.com>
2016-04-10 14:22:29 +0800

    xiaomi index

--- xiaomi/js/index.js
+++ xiaomi/js/index.js
@@ -13,7 +13,27 @@ $("#header").find(".txt").blur(function(){  //失去焦点
 
 var timer=null;
 $(".header-nav li").hover(function(){	
+	clearInterval(timer);  
+	$(".details").animate({height:"230px"}).find("li").eq($(this).index()).show().siblings().hide();
+},function(){
+	fnHide();
+});
+
+$(".details li").hover(function(){
 	clearInterval(timer);
+},function(){
+	fnHide();
+});
+
+function fnHide(){
+	timer=setTimeout(function(){
+		$(".details").animate({height:"0px"}).find("li").hide();
+	},200);
+};
+/*移入的时候清楚延迟消失的定时器*/
+/*var timer=null;
+$(".header-nav li").hover(function(){	
+	clearInterval(timer);  
 	$(".details li").eq($(this).index()).stop().animate({height:"210px"}).siblings().css("height","0px");
 },function(){
 	fnHide();
@@ -30,4 +50,4 @@ function fnHide(){
 	timer=setTimeout(function(){
 		$(".details li").css("height","0px");
 	},200);
-};
+};*/
