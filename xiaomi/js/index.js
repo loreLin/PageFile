698698a60da1fc5102f154f48a914827203aa057
loreLin <loreLinF@163.com>
2016-04-10 21:27:03 +0800

    xiaomi index

--- xiaomi/js/index.js
+++ xiaomi/js/index.js
@@ -12,7 +12,7 @@ $("#header").find(".txt").blur(function(){  //失去焦点
 
 
 var timer=null;
-$(".header-nav li").hover(function(){	
+$(".header-nav li").not(".noselect").hover(function(){	
 	clearInterval(timer);  
 	$(".details").animate({height:"230px"}).find("li").eq($(this).index()).show().siblings().hide();
 },function(){
@@ -51,3 +51,53 @@ function fnHide(){
 		$(".details li").css("height","0px");
 	},200);
 };*/
+
+
+/*banner*/
+var oTimer=null;
+$("#banner .banner-wrap").hover(function(){
+	clearInterval(oTimer);
+	$("#banner .banner-wrap .btnLeft,#banner .banner-wrap .btnRight").show();
+},function(){
+	$("#banner .banner-wrap .btnLeft,#banner .banner-wrap .btnRight").hide();
+	oTimer=setInterval(fnNext,2000);
+})
+
+var _index=0;
+$("#banner .banner-wrap .btnRight").click(function(){
+	fnNext();
+})
+
+$("#banner .banner-wrap .btnLeft").click(function(){
+	_index--;
+	if (_index<0) {
+		_index=$(".banner-wrap-pic li").size()-1;
+	};
+	fnDo();
+})
+
+oTimer=setInterval(fnNext,2000);
+
+function fnNext(){
+	_index++;
+	if (_index==$(".banner-wrap-pic li").size()) {
+		_index=0;
+	};
+	fnDo();
+}
+
+function fnDo(){
+	$(".banner-wrap-pic li").css("left","0px").eq(_index).fadeIn().siblings().fadeOut();
+	$(".banner-wrap ol li").eq(_index).addClass("active").siblings().removeClass("active");
+}
+
+$(".banner-wrap ol li").click(function(){
+	if (_index!=$(this).index()) {
+		var oneWidth=$(".banner-wrap-pic li").width();
+		var m=_index>$(this).index()?-1:1;
+		$(".banner-wrap-pic li").eq(_index).animate({left:-m*oneWidth},function(){$(this).hide();
+		}).end().eq($(this).index()).show().css("left",m*oneWidth).animate({left:0});
+		_index=$(this).index();
+		$(this).addClass("active").siblings().removeClass("active");
+	};
+})
