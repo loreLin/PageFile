fb1f7295a313782b7f4846ff0a1be43df0907083
loreLin <loreLinF@163.com>
2016-08-08 21:10:58 +0800

    node test

--- node/crawler/crawler/app.js
+++ node/crawler/crawler/app.js
@@ -1,12 +1,20 @@
 var request = require('request');
 var cheerio = require('cheerio');
-
-request('http://chanyouji.com/', function (error, response, body) {
+var url = ' http://chanyouji.com/';
+request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         $ = cheerio.load(body);
+       $(".page-container a").each(function(a,b){
+          if($(b).attr("class")!=="user"){
 
-        console.log($);
 
+
+               console.log($(b).attr("href").substr(7,13));
+             console.log();
+          }
+
+    });
     }
-
 });
+
+
